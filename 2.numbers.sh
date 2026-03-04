12dd7486c95d467f7c96d8fbf224b52ac3a9800e
VladimirBelaz <telemetristika@gmail.com>
2026-03-04 12:40:08 +0300

    Update 2.numbers.sh

--- 2.numbers.sh
+++ 2.numbers.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 n=$1
-m=$((n/2))
+mid=$((n/2))
 
-echo "mult: $(seq 1 $m | paste -sd "*" | bc)"
-echo "sum: $(seq $((m+1)) $n | paste -sd "+" | bc)"
+echo "mult: $(seq 1 $mid | paste -sd "*" | bc)"
+
+if [ $((n%2)) -eq 0 ]
+then
+  start=$((mid+1))
+else
+  start=$((mid+2))
+fi
+
+echo "sum: $(seq $start $n | paste -sd "+" | bc)"
